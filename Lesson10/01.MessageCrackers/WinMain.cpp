523c5756040c5a814e77fdda52013bbc84968247
Maxim <maxshaptala@gmail.com>
2016-02-18 12:54:28 +0200

    Добавлен пример HANDLE_MSG

--- Lesson10/01.MessageCrackers/WinMain.cpp
+++ Lesson10/01.MessageCrackers/WinMain.cpp
@@ -9,21 +9,36 @@ int APIENTRY WinMain(HINSTANCE hInst, HINSTANCE hPrev, LPSTR lpszCmdOpt, int nSh
 	return DialogBox(hInst, MAKEINTRESOURCE(IDD_DIALOG1), NULL, DlgProc);
 }
 
-INT_PTR CALLBACK DlgProc(HWND hDlg, UINT msg, WPARAM wParam, LPARAM lParam)
+void Cls_OnClose(HWND hDlg)
 {
-	switch (msg)
-	{	
-	case WM_CLOSE:
-		EndDialog(hDlg, 0);
-		return TRUE;
-	case WM_COMMAND:
-		switch (LOWORD(wParam))
-		{
+	EndDialog(hDlg, 0);
+}
+
+void Cls_OnCommand(HWND hDlg, int id, HWND hwndCtl, UINT codeNotify)
+{
+	switch (id)
+	{
 		case IDOK:
 		case IDCANCEL:
 			SendMessage(hDlg, WM_CLOSE, NULL, NULL);
-		}
-		return TRUE;		
+			break;
+	}
+}
+
+void Cls_OnLButtonDown(HWND hDlg, BOOL fDoubleClick, int x, int y, UINT keyFlags)
+{
+	TCHAR buff[50];
+	wsprintf(buff, TEXT("x: %d, y: %d"), x, y);
+	SetWindowText(hDlg, buff);
+}
+
+INT_PTR CALLBACK DlgProc(HWND hDlg, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+	switch (msg)
+	{
+		HANDLE_MSG(hDlg, WM_CLOSE, Cls_OnClose);
+		HANDLE_MSG(hDlg, WM_COMMAND, Cls_OnCommand);
+		HANDLE_MSG(hDlg, WM_LBUTTONDOWN, Cls_OnLButtonDown);
 	}
 	return FALSE;
 }
