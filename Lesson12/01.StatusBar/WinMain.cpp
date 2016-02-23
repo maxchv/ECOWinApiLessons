b75489d1b734b960b35d7bf758b84b5ec60c9242
Maxim <maxshaptala@gmail.com>
2016-02-23 13:51:31 +0200

    Добавлен пример работы со строкой состояния

--- Lesson12/01.StatusBar/WinMain.cpp
+++ Lesson12/01.StatusBar/WinMain.cpp
@@ -27,6 +27,37 @@ void Cls_OnCommand(HWND hDlg, int id, HWND hwndCtl, UINT codeNotify)
 	}
 }
 
+BOOL Cls_OnInitDialog(HWND hwnd, HWND hwndFocus, LPARAM lParam)
+{
+	// Создание строки состояния
+	HWND hStatus = CreateStatusWindow(WS_CHILD | WS_VISIBLE | CCS_BOTTOM | SBARS_TOOLTIPS,
+		                              TEXT("Все в порядке "), 
+									  hwnd, IDD_STATUSBAR);
+	
+	RECT r;
+	GetWindowRect(hwnd, &r);
+	int width = r.right - r.left;
+	
+	// Добавление трех секций
+	int parts[4] = { width / 3, width * 2 / 3, width*2/3+80, -1 };
+	SendMessage(hStatus, SB_SETPARTS, 4, LPARAM(parts));
+	
+	// Добавляем текст
+	SendMessage(hStatus, SB_SETTEXT, 1 | SBT_NOBORDERS, LPARAM(TEXT("Вторая секция")));	
+
+	SendMessage(hStatus, SB_SETTEXT, 2 | SBT_POPOUT, LPARAM(TEXT("Третья секция")));
+
+	//SendMessage(hStatus, SB_SETBKCOLOR, 0, LPARAM(RGB(0, 0, 255)));
+
+	// Задание иконки в статусбаре
+	HICON hIcon = LoadIcon(GetModuleHandle(0), MAKEINTRESOURCE(IDI_ICON1));
+	SendMessage(hStatus, SB_SETICON, 3, LPARAM(hIcon));
+
+	// Всплывающая подсказка
+	SendMessage(hStatus, SB_SETTIPTEXT, 0, LPARAM(TEXT("Всплывающая подсказка")));
+
+	return TRUE;
+}
 
 INT_PTR CALLBACK DlgProc(HWND hDlg, UINT msg, WPARAM wParam, LPARAM lParam)
 {
@@ -34,6 +65,7 @@ INT_PTR CALLBACK DlgProc(HWND hDlg, UINT msg, WPARAM wParam, LPARAM lParam)
 	{
 		HANDLE_MSG(hDlg, WM_CLOSE, Cls_OnClose);
 		HANDLE_MSG(hDlg, WM_COMMAND, Cls_OnCommand);
+		HANDLE_MSG(hDlg, WM_INITDIALOG, Cls_OnInitDialog);
 	}
 	return FALSE;
 }
