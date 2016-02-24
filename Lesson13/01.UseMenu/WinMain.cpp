32a522579bbfda598f49113404e95fed50932407
Maxim <maxshaptala@gmail.com>
2016-02-24 13:55:45 +0200

    Добавлен пример работы с меню

--- Lesson13/01.UseMenu/WinMain.cpp
+++ Lesson13/01.UseMenu/WinMain.cpp
@@ -7,6 +7,8 @@
 
 INT_PTR CALLBACK DlgProc(HWND, UINT, WPARAM, LPARAM);
 
+HWND hStatus;
+
 int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpszCmdArgs, int nCmdOpt)
 {
 	INITCOMMONCONTROLSEX icc;
@@ -18,11 +20,40 @@ int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpszCmdArgs
 
 BOOL Cls_OnInitDialog(HWND hwnd, HWND hwndFocus, LPARAM lParam)
 {
+	HMENU hMenu = LoadMenu(GetModuleHandle(0), MAKEINTRESOURCE(IDR_MENU1));
+	SetMenu(hwnd, hMenu);
+
+	hStatus = CreateStatusWindow(WS_CHILD | WS_VISIBLE | SBARS_SIZEGRIP | SBARS_TOOLTIPS, TEXT("Ready"), hwnd, IDC_STATUSBAR);
+
+	HMENU hEdit = GetSubMenu(hMenu, 1);
+
+	EnableMenuItem(hEdit, 0, MF_BYPOSITION | MF_GRAYED);
+	
+	DrawMenuBar(hwnd);
+
 	return FALSE;
 }
 
 void Cls_OnMenuSelect(HWND hwnd, HMENU hmenu, int item, HMENU hmenuPopup, UINT flags)
 {
+	switch (item) 
+	{
+	case ID_FILE_NEW:
+		SendMessage(hStatus, SB_SETTEXT, 0, (LPARAM)TEXT("Create new file"));
+		break;
+	case ID_FILE_OPEN:
+		SendMessage(hStatus, SB_SETTEXT, 0, (LPARAM)TEXT("Open file"));
+		break;
+	case ID_FILE_SAVE:
+		SendMessage(hStatus, SB_SETTEXT, 0, (LPARAM)TEXT("Save file"));
+		break;
+	case ID_FILE_EXIT:
+		SendMessage(hStatus, SB_SETTEXT, 0, (LPARAM)TEXT("Close the program"));
+		break;
+	default:
+		SendMessage(hStatus, SB_SETTEXT, 0, (LPARAM)TEXT("Ready"));
+		break;
+	}
 }
 
 void Cls_OnClose(HWND hDlg)
@@ -35,6 +66,7 @@ void Cls_OnCommand(HWND hwnd, int id, HWND hwndCtl, UINT codeNotify)
 	switch (id) 
 	{
 	case IDOK:
+	case ID_FILE_EXIT:
 		SendMessage(hwnd, WM_CLOSE, NULL, NULL);
 		break;
 	}
