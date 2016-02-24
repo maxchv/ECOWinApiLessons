4e02c5da6913ddb8fb8690b6f7aabb5793e9c600
Maxim <maxshaptala@gmail.com>
2016-02-24 11:46:56 +0200

    Пересоздан проект т.к. предыдущий содержал ошибке

--- Lesson13/01.UseMenu/WinMain.cpp
+++ Lesson13/01.UseMenu/WinMain.cpp
@@ -13,13 +13,11 @@ int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpszCmdArgs
 	icc.dwSize = sizeof(icc);
 	icc.dwICC = ICC_WIN95_CLASSES;
 	InitCommonControlsEx(&icc);
-
+	return DialogBox(hInstance, MAKEINTRESOURCE(IDD_DIALOG1), NULL, DlgProc);
 }
 
 BOOL Cls_OnInitDialog(HWND hwnd, HWND hwndFocus, LPARAM lParam)
 {
-	//HMENU hMenu = LoadMenu(GetModuleHandle(0), MAKEINTRESOURCE(IDR_MENU1));
-	//SetMenu(hwnd, hMenu);
 	return FALSE;
 }
 
@@ -27,21 +25,27 @@ void Cls_OnMenuSelect(HWND hwnd, HMENU hmenu, int item, HMENU hmenuPopup, UINT f
 {
 }
 
-void Cls_OnClose(HWND hDlg) 
+void Cls_OnClose(HWND hDlg)
 {
 	EndDialog(hDlg, 0);
 }
 
 void Cls_OnCommand(HWND hwnd, int id, HWND hwndCtl, UINT codeNotify)
 {
+	switch (id) 
+	{
+	case IDOK:
+		SendMessage(hwnd, WM_CLOSE, NULL, NULL);
+		break;
+	}
 }
 
 INT_PTR CALLBACK DlgProc(HWND hDlg, UINT msg, WPARAM wParam, LPARAM lParam)
 {
-	switch(msg)
+	switch (msg)
 	{
-		//HANDLE_MSG(hDlg, WM_INITDIALOG, Cls_OnInitDialog);
-		HANDLE_MSG(hDlg, WM_MENUSELECT, Cls_OnMenuSelect);		
+		HANDLE_MSG(hDlg, WM_INITDIALOG, Cls_OnInitDialog);
+		HANDLE_MSG(hDlg, WM_MENUSELECT, Cls_OnMenuSelect);
 		HANDLE_MSG(hDlg, WM_COMMAND, Cls_OnCommand);
 		HANDLE_MSG(hDlg, WM_CLOSE, Cls_OnClose);
 	}
