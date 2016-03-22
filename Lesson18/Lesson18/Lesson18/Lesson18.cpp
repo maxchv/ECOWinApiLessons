4669314c35e46df4602021ce6668aef2c1c74df0
Maxim <maxshaptala@gmail.com>
2016-03-22 13:56:02 +0200

    Добавлен пример работы с toolbar

--- Lesson18/Lesson18/Lesson18/Lesson18.cpp
+++ Lesson18/Lesson18/Lesson18/Lesson18.cpp
@@ -5,14 +5,14 @@
 
 // Панель инструментов (Toolbar Control)
 /*
-	Панель инструментов — строка горизонтально (или вертикально) расположенных графических кнопок, иногда 
-	объединенных в группы. Разбивка на группы осуществляется программным путем, в соответствии с 
-	назначением кнопок Изображения кнопок хранятся в общем растровом изображении, входящем в ресурсы приложения. 
-	Если щелкнуть кнопку, она пошлет командное сообщение, подобно меню и быстрым клавишам. 
-	Для обновления состояния кнопок служат обработчики обновления командного пользовательского интерфейса, 
-	вызываемые каркасом приложений для изменения внешнего вида кнопок
+    Панель инструментов — строка горизонтально (или вертикально) расположенных графических кнопок, иногда 
+    объединенных в группы. Разбивка на группы осуществляется программным путем, в соответствии с 
+    назначением кнопок Изображения кнопок хранятся в общем растровом изображении, входящем в ресурсы приложения. 
+    Если щелкнуть кнопку, она пошлет командное сообщение, подобно меню и быстрым клавишам. 
+    Для обновления состояния кнопок служат обработчики обновления командного пользовательского интерфейса, 
+    вызываемые каркасом приложений для изменения внешнего вида кнопок
 
-	https://msdn.microsoft.com/en-us/library/windows/desktop/bb760443(v=vs.85).aspx
+    https://msdn.microsoft.com/en-us/library/windows/desktop/bb760443(v=vs.85).aspx
 */
 
 // Шаг 1. Загрузка и регистрация класса окна Toolbar.
@@ -25,7 +25,7 @@ iccx.dwSize = sizeof(INITCOMMONCONTROLSEX);
 iccx.dwICC = ICC_BAR_CLASSES;
 if (!InitCommonControlsEx(&iccx))
 {
-	return FALSE;
+    return FALSE;
 }
 */
 
@@ -37,59 +37,62 @@ if (!InitCommonControlsEx(&iccx))
 Можно задать следующие стили:
 
 TBSTYLE_FLAT		Текст кнопок отображается под картинками.
-					Для предотвращаения проблем с перерисовкой, стиль должен быть
-					установлен перед отображением панели инструментов.
-TBSTYLE_TOOLTIP		Вывод краткого описания кнопки в окне элемента управления Tool Tip
+                    Для предотвращаения проблем с перерисовкой, 
+                    стиль должен быть
+                    установлен перед отображением панели инструментов.
+TBSTYLE_TOOLTIP		Вывод краткого описания кнопки в окне элемента управления
+                    Tool Tip
 TBSTYLE_LIST		Текст кнопок появляется справа от картинок.
-TBSTYLE_TRANSPARENT Создает прозрачную панель инструментов (но не прозрачные кнопки).
-					Текст кнопок отображается под картинками.
+TBSTYLE_TRANSPARENT Создает прозрачную панель инструментов 
+                    (но не прозрачные кнопки).
+                    Текст кнопок отображается под картинками.
 TBSTYLE_ALTDRAG		Если не указан стиль TBSTYLE_ALTDRAG, то пользователь может 
-					передвигать кнопки по поверхности Toolbar левой клавишей мыши 
-					при нажатой клавише <Shift>. Если же этот стиль указан, для 
-					перемещения кнопок используется клавиша <Alt>. В любом случае 
-					кнопки можно передвигать только тогда, когда указан стиль CCS_ADJUSTABLE
+                    передвигать кнопки по поверхности Toolbar левой клавишей мыши 
+                    при нажатой клавише <Shift>. Если же этот стиль указан, для 
+                    перемещения кнопок используется клавиша <Alt>. В любом случае 
+                    кнопки можно передвигать только тогда, когда указан стиль CCS_ADJUSTABLE
 TBSTYLE_WRAPABLE	Окно Toolbar может состоять из нескольких строк. Новые строки 
-					создаются в том случае, если все кнопки не помещаются в одной строке
+                    создаются в том случае, если все кнопки не помещаются в одной строке
 
 https://msdn.microsoft.com/en-us/library/windows/desktop/bb760439(v=vs.85).aspx#TBSTYLE_LIST
 
 Дополнительные стили:
 
-CCS_ADJUSTABLE		Если указан этот стиль, пользователь может изменять конфигурацию 
-					элемента управления
+CCS_ADJUSTABLE		Если указан этот стиль, пользователь может изменять 
+                    конфигурацию элемента управления
 CCS_BOTTOM			Элемент управления должен быть расположен в нижней части внутренней 
-					области окна
+                    области окна
 CCS_TOP				Элемент управления должен быть расположен в верхней части внутренней 
-					области окна
+                    области окна
 
 https://msdn.microsoft.com/en-us/library/windows/desktop/bb775498(v=vs.85).aspx#CCS_ADJUSTABLE
 
 Пример:
-	HWND hToolbar = CreateWindow(TOOLBARCLASSNAME, 0,
-		TBSTYLE_FLAT | CCS_ADJUSTABLE | CCS_NODIVIDER | WS_CHILD | WS_VISIBLE,
-		0, 0, 0, 0,
-		hWnd, (HMENU)IDC_TOOLBAR, GetModuleHandle(0), 0);
+    HWND hToolbar = CreateWindow(TOOLBARCLASSNAME, 0,
+        TBSTYLE_FLAT | CCS_ADJUSTABLE | CCS_NODIVIDER | WS_CHILD | WS_VISIBLE,
+        0, 0, 0, 0,
+        hWnd, (HMENU)IDC_TOOLBAR, GetModuleHandle(0), 0);
 */
 
 // Шаг 3. Настроить и добавить кнопки в панель инструментов.
 /*
 3.1 После создания необходимо отправить сообщение TB_BUTTONSTRUCTSIZE в панель инструментов. 
-Если приложение использует функцию CreateWindowEx для создания панели инструментов, 
+Если приложение использует функцию CreateWindow для создания панели инструментов, 
 необходимо отправить это сообщение перед сообщениями TB_ADDBITMAP или TB_ADDBUTTONS. 
 
 Посылаем сообщение TB_BUTTONSTRUCTSIZE, указывая размер структуры TBBUTTON.
 
 Пример:
-	SendMessage(hToolbar, TB_BUTTONSTRUCTSIZE, (WPARAM)sizeof(TBBUTTON), 0);
-	
+    SendMessage(hToolbar, TB_BUTTONSTRUCTSIZE, (WPARAM)sizeof(TBBUTTON), 0);
+    
 3.2 Добавить изображения (TBADDBITMAP, TB_ADDBITMAP)
 
 Затем заполняем структуру TBADDBITMAP:
-	typedef struct
-	{
-		HINSTANCE hInst;				// идентификатор приложения
-		UINT nID;						// nID - идентификатор изображения кнопок
-	} TBADDBITMAP, *LPTBADDBITMAP;
+    typedef struct
+    {
+        HINSTANCE hInst;				// идентификатор приложения
+        UINT nID;						// nID - идентификатор изображения кнопок
+    } TBADDBITMAP, *LPTBADDBITMAP;
 
 Можно использовать предопределенные системные изображения, тогда hInst должен быть
 задан HINST_COMMCTRL, а значение nID выбирается из списка
@@ -100,15 +103,15 @@ IDB_STD_SMALL_COLOR				Стандартные маленькие цветные изображения
 Другие стандартные значения nID: https://msdn.microsoft.com/en-us/library/windows/desktop/bb760475(v=vs.85).aspx
  
 Пример:
-	TBADDBITMAP tbAddBmp = {0};
-	tbAddBmp.hInst = HINST_COMMCTRL;
-	tbAddBmp.nID = IDB_STD_SMALL_COLOR;
+    TBADDBITMAP tbAddBmp = {0};
+    tbAddBmp.hInst = HINST_COMMCTRL;
+    tbAddBmp.nID = IDB_STD_SMALL_COLOR;
 
 Адрес заполненной структуры типа TBADDBITMAP передаем в 
 качестве параметров сообщения TB_ADDBITMAP.
 
 Пример:
-	SendMessage(hToolbar, TB_ADDBITMAP, 0, (WPARAM)&tbAddBmp);
+    SendMessage(hToolbar, TB_ADDBITMAP, 0, (WPARAM)&tbAddBmp);
 
 3.3 Добавление кнопок (TBBUTTON, TB_ADDBUTTONS)
 
@@ -121,16 +124,16 @@ IDB_STD_SMALL_COLOR				Стандартные маленькие цветные изображения
 
 typedef struct _TBBUTTON
 {
-	int   iBitmap;			// индекс изображения кнопки
-	int   idCommand;		// идентификатор, который будет передаваться родительскому окну с 
-							// сообщением WM_COMMAND
-							// Если элемент массива структур TBBUTTON описывает разделитель 
-							// группы кнопок, в поле idCommand нужно записать нулевое значение.
-	BYTE  fsState;			// флаг исходного состояния кнопки
-	BYTE  fsStyle;			// стиль кнопки
-	DWORD dwData;			// дополнительные данные, которые будут хранится в описании 
-							// кнопки и использоваться при необходимости
-	int   iString;			// индекс текстовой метки кнопки
+    int   iBitmap;			// индекс изображения кнопки
+    int   idCommand;		// идентификатор, который будет передаваться родительскому окну с 
+                            // сообщением WM_COMMAND
+                            // Если элемент массива структур TBBUTTON описывает разделитель 
+                            // группы кнопок, в поле idCommand нужно записать нулевое значение.
+    BYTE  fsState;			// флаг исходного состояния кнопки
+    BYTE  fsStyle;			// стиль кнопки
+    DWORD dwData;			// дополнительные данные, которые будут хранится в описании 
+                            // кнопки и использоваться при необходимости
+    int   iString;			// индекс текстовой метки кнопки
 } TBBUTTON, NEAR * PTBBUTTON, FAR * LPTBBUTTON;
 typedef const TBBUTTON FAR * LPCTBBUTTON;
 
@@ -139,26 +142,26 @@ typedef const TBBUTTON FAR * LPCTBBUTTON;
 соответствующих командам меню File и Edit, откуда мы и позаимствуем необходимые изображения. 
 
 Идентификаторы кнопок вполне отражают их назначение:
-	STD_CUT,		STD_COPY,		STD_PASTE,		STD_UNDO,		STD_REDOW, 
-	STD_DELETE,		STD_FILEMEW,	STD_PRINTPRE,	STD_PROPERTIES, STD_HELP, STD_FIND, 
-	STD_FILEOPEN,	STD_FILESAVE,	STD_REPLACE,	STD_PRINT.	
+    STD_CUT,		STD_COPY,		STD_PASTE,		STD_UNDO,		STD_REDOW, 
+    STD_DELETE,		STD_FILENEW,	STD_PRINTPRE,	STD_PROPERTIES, STD_HELP, STD_FIND, 
+    STD_FILEOPEN,	STD_FILESAVE,	STD_REPLACE,	STD_PRINT.	
 
 Другие идентификаторы
-	https://msdn.microsoft.com/en-us/library/windows/desktop/bb760433(v=vs.85).aspx
+    https://msdn.microsoft.com/en-us/library/windows/desktop/bb760433(v=vs.85).aspx
 
 Флаг исходного состояния кнопки fsState:
 TBSTATE_ENABLED			Кнопка находится в разблокированном состоянии. 
-						Если этот флаг не установлен, кнопка заблокирована и отображается 
-						серым цветом
+                        Если этот флаг не установлен, кнопка заблокирована и отображается 
+                        серым цветом
 TBSTATE_CHECKED			Кнопка изображается в нажатом состоянии. Этот флаг используется 
-						для кнопок с фиксацией
+                        для кнопок с фиксацией
 TBSTATE_HIDDEN			Скрытая кнопка, не отображается
 TBSTATE_INDETERMINATE	Кнопка отображается серым цветом
 TBSTATE_PRESSED			Кнопка изображается в нажатом состоянии
 TBSTATE_WRAP			Кнопки, расположенные после той, что имеет флаг TBSTATE_WRAP, 
-						отображаются на новой строке. Таким образом, вы можете создать 
-						многострочный Toolbar . Этот флаг можно указывать только тогда, 
-						когда установлен флаг TBSTATE_ENABLED
+                        отображаются на новой строке. Таким образом, вы можете создать 
+                        многострочный Toolbar . Этот флаг можно указывать только тогда, 
+                        когда установлен флаг TBSTATE_ENABLED
 
 Другие состояния: https://msdn.microsoft.com/en-us/library/windows/desktop/bb760437(v=vs.85).aspx
 
@@ -174,35 +177,35 @@ TBSTYLE_SEP			Разделитель между группами кнопок
 
 Пример сосздания набора из 7 кнопок:
 
-	const int numButtons = 7;
-	TBBUTTON tbButtons[numButtons] =
-	{
-		{ STD_FILENEW, NULL, TBSTATE_ENABLED,
-			BTNS_AUTOSIZE, 0, 0, (INT_PTR)TEXT("New") },
-		{ STD_FILEOPEN, NULL, TBSTATE_ENABLED,
-			BTNS_AUTOSIZE, 0, 0, (INT_PTR)TEXT("Open") },
-		{ STD_FILESAVE, NULL, 0,
-			BTNS_AUTOSIZE, 0, 0, (INT_PTR)TEXT("Save") },
-		{ 0, NULL, 0, TBSTYLE_SEP, 0, 0, (INT_PTR)TEXT("") }, // разделитель
-		{ STD_COPY, NULL, TBSTATE_ENABLED,
-			BTNS_AUTOSIZE, 0, 0, (INT_PTR)TEXT("Copy") },
-		{ STD_CUT, NULL, TBSTATE_ENABLED,
-			BTNS_AUTOSIZE, 0, 0, (INT_PTR)TEXT("Cut") },
-		{ STD_PASTE, NULL, TBSTATE_ENABLED,
-			BTNS_AUTOSIZE, 0, 0, (INT_PTR)TEXT("Paste") }
-	};
+    const int numButtons = 7;
+    TBBUTTON tbButtons[numButtons] =
+    {
+        { STD_FILENEW, NULL, TBSTATE_ENABLED,
+            BTNS_AUTOSIZE, 0, 0, (INT_PTR)TEXT("New") },
+        { STD_FILEOPEN, NULL, TBSTATE_ENABLED,
+            BTNS_AUTOSIZE, 0, 0, (INT_PTR)TEXT("Open") },
+        { STD_FILESAVE, NULL, 0,
+            BTNS_AUTOSIZE, 0, 0, (INT_PTR)TEXT("Save") },
+        { 0, NULL, 0, TBSTYLE_SEP, 0, 0, (INT_PTR)TEXT("") }, // разделитель
+        { STD_COPY, NULL, TBSTATE_ENABLED,
+            BTNS_AUTOSIZE, 0, 0, (INT_PTR)TEXT("Copy") },
+        { STD_CUT, NULL, TBSTATE_ENABLED,
+            BTNS_AUTOSIZE, 0, 0, (INT_PTR)TEXT("Cut") },
+        { STD_PASTE, NULL, TBSTATE_ENABLED,
+            BTNS_AUTOSIZE, 0, 0, (INT_PTR)TEXT("Paste") }
+    };
 
 Добавляем кнопки к панели инструментов:
-	
-	SendMessage(hToolbar, TB_ADDBUTTONS, numButtons, (LPARAM)tbButtons);
+    
+    SendMessage(hToolbar, TB_ADDBUTTONS, numButtons, (LPARAM)tbButtons);
 */
 
-// Шаг 4. Передать сообщение автоматически разместит и отобразить 
+// Шаг 4. Передать сообщение автоматически разместить и отобразить 
 //        панель инструментов. (TB_AUTOSIZE)
 /*
 
 Пример:
-	SendMessage(hToolbar, TB_AUTOSIZE, 0, 0);
+    SendMessage(hToolbar, TB_AUTOSIZE, 0, 0);
 */
 
 // Шаг 5. Обработать сообщения от панели инструментов
@@ -211,14 +214,14 @@ Toolbar посылает в родительское окно сообщения WM_COMMAND и WM_NOTIFY.
 
 Параметр lParam сообщения WM_NOTIFY содержит указатель на структуру 
 TBNOTIFY (содержащую в самом начале структуру NMHDR ):
-	typedef struct
-	{
-		NMHDR    hdr;
-		int      iItem;
-		TBBUTTON tbButton;
-		int      cchText;
-		LPTSTR   pszText;
-	} TBNOTIFY, FAR * LPTBNOTIFY;
+    typedef struct
+    {
+        NMHDR    hdr;
+        int      iItem;
+        TBBUTTON tbButton;
+        int      cchText;
+        LPTSTR   pszText;
+    } TBNOTIFY, FAR * LPTBNOTIFY;
 
 В этой структуре поле iItem содержит номер кнопки, от которой пришло извещение 
 (нумерация кнопок начинается с нуля).
@@ -230,6 +233,6 @@ TBNOTIFY (содержащую в самом начале структуру NMHDR ):
 */
 int _tmain(int argc, _TCHAR* argv[])
 {
-	return 0;
+    return 0;
 }
 
--- Lesson18/Lesson18/ToolbarExample/MainDialog.cpp
+++ Lesson18/Lesson18/ToolbarExample/MainDialog.cpp
@@ -19,7 +19,49 @@ void MainDialog::Cls_OnClose(HWND hwnd)
 
 BOOL MainDialog::Cls_OnInitDialog(HWND hwnd, HWND hwndFocus, LPARAM lParam)
 {
+	// создаем окно панели инструментов
+	hToolbar = CreateWindow(TOOLBARCLASSNAME,
+							NULL,
+							TBSTYLE_FLAT | CCS_TOP | CCS_NODIVIDER | WS_CHILD | WS_VISIBLE,
+							0, 0, 0, 0,
+							hwnd,
+							(HMENU)IDC_TOOLBAR,
+							GetModuleHandle(0),
+							NULL);
+	// использовать структуру TBBUTTON дл€ хранени€ данных кнопок панели инструментов
+	SendMessage(hToolbar, TB_BUTTONSTRUCTSIZE, WPARAM(sizeof(TBBUTTON)), 0);
 	
+	// —оздание и настройка структуры дл€ панели управлени€
+	// ”казать исочник изображений дл€ панели управлени€
+	TBADDBITMAP tbAddBmp = { 0 };
+	tbAddBmp.hInst = GetModuleHandle(0);// HINST_COMMCTRL;     // стандартные изображени€
+	tbAddBmp.nID = IDR_TOOLBAR1;// IDB_STD_SMALL_COLOR;  
+
+	SendMessage(hToolbar, TB_ADDBITMAP, 0, (LPARAM)&tbAddBmp);
+
+	// «аполнение панели данными
+	TBBUTTON buttons[] = {
+		//{STD_FILENEW, ID_FILE_NEW, TBSTATE_ENABLED, BTNS_AUTOSIZE, { 0 }, 0, (INT_PTR)TEXT("New")},
+		{ 0, ID_FILE_NEW, TBSTATE_ENABLED, BTNS_AUTOSIZE,{ 0 }, 0, (INT_PTR)TEXT("New") },
+		//{STD_FILEOPEN, ID_FILE_OPEN, TBSTATE_ENABLED, BTNS_AUTOSIZE,{ 0 }, 0, (INT_PTR)TEXT("Open") },
+		{ 1, ID_FILE_OPEN, TBSTATE_ENABLED, BTNS_AUTOSIZE,{ 0 }, 0, (INT_PTR)TEXT("Open") },
+		//{STD_FILESAVE, ID_FILE_SAVE, TBSTATE_ENABLED, BTNS_AUTOSIZE,{ 0 }, 0, (INT_PTR)TEXT("Save") },
+		{ 2, ID_FILE_SAVE, TBSTATE_ENABLED, BTNS_AUTOSIZE,{ 0 }, 0, (INT_PTR)TEXT("Save") },
+		{ 0, NULL, 0, TBSTYLE_SEP, { 0 }, 0, (INT_PTR)TEXT("") },
+		//{ STD_COPY, ID_EDIT_COPY, TBSTATE_ENABLED, BTNS_AUTOSIZE,{ 0 }, 0, (INT_PTR)TEXT("Copy") },
+		{ 3, ID_EDIT_COPY, TBSTATE_ENABLED, BTNS_AUTOSIZE,{ 0 }, 0, (INT_PTR)TEXT("Copy") },
+		//{ STD_CUT, ID_EDIT_CUT, TBSTATE_ENABLED, BTNS_AUTOSIZE,{ 0 }, 0, (INT_PTR)TEXT("Cut") },
+		{ 4, ID_EDIT_CUT, TBSTATE_ENABLED, BTNS_AUTOSIZE,{ 0 }, 0, (INT_PTR)TEXT("Cut") },
+		//{ STD_PASTE, ID_EDIT_PASTE, TBSTATE_ENABLED, BTNS_AUTOSIZE,{ 0 }, 0, (INT_PTR)TEXT("Paste") }
+		{ 5, ID_EDIT_PASTE, TBSTATE_ENABLED, BTNS_AUTOSIZE,{ 0 }, 0, (INT_PTR)TEXT("Paste") }
+	};
+
+	int nButtons = sizeof(buttons) / sizeof(buttons[0]);
+	// ”становка панели
+	SendMessage(hToolbar, TB_ADDBUTTONS, nButtons, (LPARAM)buttons);
+
+	// ѕодбор размера панели под размер окна
+	SendMessage(hToolbar, TB_AUTOSIZE, 0, 0);
 
 	return TRUE;
 }
