5b3373fad6e9c9622cdc69ec1f38b59166a707b3
Maxim <maxshaptala@gmail.com>
2017-08-28 00:31:55 +0300

    Подготовлено к занятию

--- Lesson01/Lesson01/Lesson01.cpp
+++ Lesson01/Lesson01/Lesson01.cpp
@@ -8,7 +8,7 @@
 using namespace std;
 
 /*
-	Модуль №1  Введение в Windows программирование с использованием Win  API
+	Модуль №1  Введение в Windows программирование с использованием WinAPI
 	Занятие №1
 */
 
@@ -16,13 +16,7 @@ using namespace std;
 // тип данных wchar_t
 void ex00()
 {
-	char ch;
-	wchar_t wch;
-	cout << "wide char: " << sizeof(wch) << endl;
-	cout << "char: " << sizeof(ch) << endl;
-
-	wch = 'h';
-	wcout << wch << endl;
+	
 }
 
 // Строковые функции Unicode
@@ -35,73 +29,35 @@ void ex01()
 
 	// Строковые функции ANSI C
 	// char * strcat( char *,  const char *);
-	/*strcat(words, word1);
-	strcat(words, ", ");
-	strcat(words, word2);
-	strcat(words, "!");
-	cout << words << endl;*/
-	strcat(words, word1);
-	strcat(words, ", ");
-	strcat(words, word2);
-	strcat(words, "!");
-	cout << words << endl;
+	
 
 	// char * strchr(const char *, int);
-	char * find = strchr(words, ',');
-	find++;
-	cout << find << endl;
+
 
 	// int strcmp(const char *, const char *);
-	cout << strcmp(word1, word2) << endl;
-	cout << strcmp("word ", "word") << endl;
+	
 
 	// char * strcpy(char *, const char *);
-	strcpy(words, word1);
-	cout << words << endl;
+	
 
 	// size_t strlen(const char *);
-	cout << strlen(words) << endl;
 
-	wchar_t *uword1 = L"Hello";
-	wchar_t *uword2 = L"Word";
-	wchar_t uwords[80];
-	uwords[0] = '\0';
 
 	// Эквивалентные Unicode-функции
 	// ANSI функции, только заменить str на wcs, т.е.
 	// wide char string
-	wcscat(uwords, uword1);
-	wcscat(uwords, L", ");
-	wcscat(uwords, uword2);
-	wcscat(uwords, L"!");
-	wcout << uwords << endl;
+	
 	// wcschr(const wchar_t *, int)
 	// int wcscmp(const wchar_t *, const wchar_t *)
 	// wchar_t * wcscpy(wchar_t *, const wchar_t *);
 	// size_t wcslen(const wchar_t *);
-	cout << sizeof(uwords) << endl;
-	cout << wcslen(uword1) << endl;
+	
 }
 
 // Макросы для работы с Unicode
 void ex02()
 {
-#ifdef _UNICODE
-	cout << "using unicode" << endl;
-#else
-	cout << "using multibyte" << endl;
-#endif
-	_TCHAR * str = _TEXT("My Unicode string");
-	cout << "_TCHAR: " << sizeof(_TCHAR) << endl;
 
-	// _TEXT
-
-	// _tcscat
-	_TCHAR wstr[50];
-	wstr[0] = '\0';
-	_tcscat(wstr, str);
-	
-	// константа _UNICODE
 }
 
 // Строковые функции Windows
@@ -131,14 +87,7 @@ void ex04()
 		LPWSTR lpWideCharStr,  // указатель на буфер, куда запишется Unicode-строка 
 		int cchWideChar        // размер буфера
 	);*/
-	char *str = "string";
-	// определяется длина строки
-	int len = MultiByteToWideChar(CP_ACP, 0, str, -1, NULL, 0);
-	cout << len << endl;
-	wchar_t *wstr = new wchar_t[len];
-	MultiByteToWideChar(CP_ACP, 0, str, len, wstr, len);
-	wcout << wstr << endl;
-	delete[] wstr;
+	
 		
 	/*
 	аналогичные преобразования выполняет функция из С/С++ Run-Time Library
@@ -148,12 +97,7 @@ void ex04()
 		size_t count //максимальное число символов исходной строки
 		);
 	*/
-	wchar_t *ustr;
-	// определяется длина строки
-	len = mbstowcs(NULL, str, 0);
-	ustr = new wchar_t[len];
-	mbstowcs(ustr, str, 0);
-	wcout << ustr << endl;
+	
 }
 
 // Перекодировка строк из Unicode в ANSI
@@ -200,7 +144,7 @@ int main()
 	//ex00();
 	//ex01();
 	//ex02();
-	ex04();
+	//ex04();
 	return 0;
 }
 
